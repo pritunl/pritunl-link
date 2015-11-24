828f334173150a59ccadcc6f99ad40755420e714
Zachary Huff <zach.huff.386@gmail.com>
2015-11-24 18:43:50 -0500

    Add error types

---
+++ errortypes/errortypes.go
@@ -0,0 +1,25 @@
+package constants
+
+import (
+	"github.com/dropbox/godropbox/errors"
+)
+
+type ParseError struct {
+	errors.DropboxError
+}
+
+type ReadError struct {
+	errors.DropboxError
+}
+
+type WriteError struct {
+	errors.DropboxError
+}
+
+type UnknownError struct {
+	errors.DropboxError
+}
+
+type ExecError struct {
+	errors.DropboxError
+}
