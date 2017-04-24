2315804763df6693dc884db81da785355f9a5cb8
Zachary Huff <zach.huff.386@gmail.com>
2017-04-24 02:54:32 -0400

    Add file utils

--- utils/file.go
+++ utils/file.go
@@ -1,36 +1,193 @@
 package utils
 
 import (
+	"github.com/autoabs/autoabs/errortypes"
 	"github.com/dropbox/godropbox/errors"
-	"github.com/pritunl/pritunl-auth/errortypes"
 	"io/ioutil"
 	"os"
+	"os/exec"
 )
 
-func Read(path string) (data string, err error) {
-	dataByt, err := ioutil.ReadFile(path)
-	if err != nil {
-		err = &errortypes.ReadError{
-			errors.Wrapf(err, "utils: Failed to read '%s'", path),
-		}
+func Exists(pth string) (exists bool, err error) {
+	_, err = os.Stat(pth)
+	if err == nil {
+		exists = true
 		return
 	}
-	data = string(dataByt)
+
+	if os.IsNotExist(err) {
+		err = nil
+		return
+	}
+
+	err = errortypes.ReadError{
+		errors.Wrapf(err, "utils: Failed to stat %s", pth),
+	}
+	return
+}
+
+func ExistsDir(pth string) (exists bool, err error) {
+	stat, err := os.Stat(pth)
+	if err == nil {
+		exists = stat.IsDir()
+		return
+	}
+
+	if os.IsNotExist(err) {
+		err = nil
+		return
+	}
+
+	err = errortypes.ReadError{
+		errors.Wrapf(err, "utils: Failed to stat %s", pth),
+	}
+	return
+}
+
+func ExistsFile(pth string) (exists bool, err error) {
+	stat, err := os.Stat(pth)
+	if err == nil {
+		exists = !stat.IsDir()
+		return
+	}
+
+	if os.IsNotExist(err) {
+		err = nil
+		return
+	}
+
+	err = errortypes.ReadError{
+		errors.Wrapf(err, "utils: Failed to stat %s", pth),
+	}
+	return
+}
+
+func ExistsMkdir(pth string, perm os.FileMode) (err error) {
+	exists, err := ExistsDir(pth)
+	if err != nil {
+		return
+	}
+
+	if !exists {
+		err = os.MkdirAll(pth, perm)
+		if err != nil {
+			err = &errortypes.WriteError{
+				errors.Wrapf(err, "utils: Failed to mkdir %s", pth),
+			}
+			return
+		}
+	}
 
 	return
 }
 
-func Exists(path string) (exists bool, err error) {
-	_, err = os.Stat(path)
-	if os.IsNotExist(err) {
-		err = nil
-	} else if err != nil {
-		err = errortypes.ReadError{
-			errors.Wrapf(err, "utils: Failed to stat file '%s'", path),
+func ExistsRemove(pth string) (err error) {
+	exists, err := Exists(pth)
+	if err != nil {
+		return
+	}
+
+	if exists {
+		err = os.RemoveAll(pth)
+		if err != nil {
+			err = &errortypes.WriteError{
+				errors.Wrapf(err, "utils: Failed to rm %s", pth),
+			}
+			return
+		}
+	}
+
+	return
+}
+
+func Remove(path string) (err error) {
+	err = os.Remove(path)
+	if err != nil {
+		err = &errortypes.WriteError{
+			errors.Wrapf(err, "utils: Failed to remove '%s'", path),
 		}
 		return
-	} else {
-		exists = true
+	}
+
+	return
+}
+
+func RemoveAll(path string) (err error) {
+	err = os.RemoveAll(path)
+	if err != nil {
+		err = &errortypes.WriteError{
+			errors.Wrapf(err, "utils: Failed to remove '%s'", path),
+		}
+		return
+	}
+
+	return
+}
+
+func Copy(sourcePath, destPath string) (err error) {
+	cmd := exec.Command(
+		"/usr/bin/cp",
+		sourcePath,
+		destPath,
+	)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	if err != nil {
+		err = errortypes.ExecError{
+			errors.Wrapf(err, "package: Failed to copy %s to %s",
+				sourcePath, destPath),
+		}
+		return
+	}
+
+	return
+}
+
+func CopyAll(sourcePath, destPath string) (err error) {
+	cmd := exec.Command(
+		"/usr/bin/cp",
+		"-r",
+		sourcePath,
+		destPath,
+	)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	if err != nil {
+		err = errortypes.ExecError{
+			errors.Wrapf(err, "package: Failed to copy %s to %s",
+				sourcePath, destPath),
+		}
+		return
+	}
+
+	return
+}
+
+func ContainsDir(pth string) (hasDir bool, err error) {
+	exists, err := ExistsDir(pth)
+	if !exists {
+		return
+	}
+
+	entries, err := ioutil.ReadDir(pth)
+	if err != nil {
+		err = &errortypes.ReadError{
+			errors.Wrapf(err, "queue: Failed to read dir %s", pth),
+		}
+		return
+	}
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			hasDir = true
+			return
+		}
 	}
 
 	return
@@ -48,19 +205,7 @@ func Create(path string) (file *os.File, err error) {
 	return
 }
 
-func MkdirAll(path string) (err error) {
-	err = os.MkdirAll(path, 0755)
-	if err != nil {
-		err = &errortypes.WriteError{
-			errors.Wrapf(err, "utils: Failed to create '%s'", path),
-		}
-		return
-	}
-
-	return
-}
-
-func Write(path string, data string) (err error) {
+func CreateWrite(path string, data string) (err error) {
 	file, err := Create(path)
 	if err != nil {
 		return
