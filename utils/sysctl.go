f07943eaa691154e35e4e7c17ec3d86cd5ec62e4
Zachary Huff <zach.huff.386@gmail.com>
2017-04-24 04:15:34 -0400

    Use exec silent in sysctl

--- utils/sysctl.go
+++ utils/sysctl.go
@@ -1,27 +1,32 @@
 package utils
 
 func NetInit() (err error) {
-	err = Exec("", "sysctl", "-w", "net.ipv4.ip_forward=1")
+	err = ExecSilent(
+		"", "sysctl", "-w", "net.ipv4.ip_forward=1")
 	if err != nil {
 		return
 	}
 
-	err = Exec("", "sysctl", "-w", "net.ipv4.conf.all.send_redirects=0")
+	err = ExecSilent(
+		"", "sysctl", "-w", "net.ipv4.conf.all.send_redirects=0")
 	if err != nil {
 		return
 	}
 
-	err = Exec("", "sysctl", "-w", "net.ipv4.conf.default.send_redirects=0")
+	err = ExecSilent(
+		"", "sysctl", "-w", "net.ipv4.conf.default.send_redirects=0")
 	if err != nil {
 		return
 	}
 
-	err = Exec("", "sysctl", "-w", "net.ipv4.conf.all.accept_redirects=0")
+	err = ExecSilent(
+		"", "sysctl", "-w", "net.ipv4.conf.all.accept_redirects=0")
 	if err != nil {
 		return
 	}
 
-	err = Exec("", "sysctl", "-w", "net.ipv4.conf.default.accept_redirects=0")
+	err = ExecSilent(
+		"", "sysctl", "-w", "net.ipv4.conf.default.accept_redirects=0")
 	if err != nil {
 		return
 	}
