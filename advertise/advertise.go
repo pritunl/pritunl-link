2a090ca1a14ac3c50d3f421b866e8620fcef4cde
Zachary Huff <zach.huff.386@gmail.com>
2017-04-29 05:17:52 -0400

    Check provider in advertise

--- advertise/advertise.go
+++ advertise/advertise.go
@@ -2,6 +2,7 @@ package advertise
 
 import (
 	"github.com/pritunl/pritunl-link/cloud"
+	"github.com/pritunl/pritunl-link/config"
 	"github.com/pritunl/pritunl-link/state"
 )
 
@@ -11,9 +12,11 @@ func AdvertiseRoutes() (err error) {
 	for _, stat := range states {
 		for _, link := range stat.Links {
 			for _, network := range link.RightSubnets {
-				err = cloud.AwsAddRoute(network)
-				if err != nil {
-					return
+				if config.Config.Provider == "aws" {
+					err = cloud.AwsAddRoute(network)
+					if err != nil {
+						return
+					}
 				}
 			}
 		}
