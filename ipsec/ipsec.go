048881f21d9e2684389675211f52612246d11309
Zachary Huff <zach.huff.386@gmail.com>
2017-04-29 02:20:49 -0400

    Remove get status from ipsec

--- ipsec/ipsec.go
+++ ipsec/ipsec.go
@@ -117,47 +117,6 @@ func writeTemplates() (err error) {
 	return
 }
 
-func GetStatus() (status map[string]string, err error) {
-	status = map[string]string{}
-
-	output, err := utils.ExecOutput("", "ipsec", "status")
-	if err != nil {
-		return
-	}
-
-	for _, line := range strings.Split(output, "\n") {
-		lines := strings.SplitN(line, ":", 2)
-		if len(lines) != 2 {
-			continue
-		}
-
-		if !strings.HasSuffix(lines[0], "]") {
-			continue
-		}
-
-		connId := strings.SplitN(lines[0], "[", 2)[0]
-		connState := strings.SplitN(
-			strings.TrimSpace(lines[1]), " ", 2)[0]
-
-		switch connState {
-		case "ESTABLISHED":
-			connState = "connected"
-			break
-		case "CONNECTING":
-			connState = "connecting"
-			break
-		default:
-			connState = "disconnected"
-		}
-
-		if _, ok := status[connId]; !ok {
-			status[connId] = connState
-		}
-	}
-
-	return
-}
-
 func Deploy() (err error) {
 	err = clearDir()
 	if err != nil {
