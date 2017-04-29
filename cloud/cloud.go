c178da0e4cc5e8f63e85cb28713b6d826684581b
Zachary Huff <zach.huff.386@gmail.com>
2017-04-29 03:39:45 -0400

    Add cloud

---
+++ cloud/cloud.go
@@ -0,0 +1,70 @@
+package cloud
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws/ec2metadata"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/pritunl-link/errortypes"
+)
+
+type awsMetaData struct {
+	Region     string
+	InstanceId string
+	VpcId      string
+}
+
+func awsGetMetaData() (data *awsMetaData, err error) {
+	sess, err := session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	})
+	if err != nil {
+		err = &errortypes.RequestError{
+			errors.Wrap(err, "advertise: Failed to create AWS session"),
+		}
+		return
+	}
+
+	ec2metadataSvc := ec2metadata.New(sess)
+
+	region, err := ec2metadataSvc.Region()
+	if err != nil {
+		err = &errortypes.RequestError{
+			errors.Wrap(err, "advertise: Failed to get AWS region"),
+		}
+		return
+	}
+
+	instanceId, err := ec2metadataSvc.GetMetadata("instance-id")
+	if err != nil {
+		err = &errortypes.RequestError{
+			errors.Wrap(err, "advertise: Failed to get EC2 instance ID"),
+		}
+		return
+	}
+
+	macAddr, err := ec2metadataSvc.GetMetadata("mac")
+	if err != nil {
+		err = &errortypes.RequestError{
+			errors.Wrap(err, "advertise: Failed to get EC2 MAC address"),
+		}
+		return
+	}
+
+	vpcId, err := ec2metadataSvc.GetMetadata(
+		fmt.Sprintf("network/interfaces/macs/%s/vpc-id", macAddr))
+	if err != nil {
+		err = &errortypes.RequestError{
+			errors.Wrap(err, "advertise: Failed to get EC2 VPC ID"),
+		}
+		return
+	}
+
+	data = &awsMetaData{
+		Region:     region,
+		InstanceId: instanceId,
+		VpcId:      vpcId,
+	}
+
+	return
+}
