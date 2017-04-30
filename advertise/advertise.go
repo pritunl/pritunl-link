9a9865f8e02e9d9c9ed7ee2c76bdf61c64d8b713
Zachary Huff <zach.huff.386@gmail.com>
2017-04-30 19:56:29 -0400

    Write routes to var directory

--- advertise/advertise.go
+++ advertise/advertise.go
@@ -1,16 +1,25 @@
 package advertise
 
 import (
+	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/pritunl-link/config"
+	"github.com/pritunl/pritunl-link/constants"
+	"github.com/pritunl/pritunl-link/errortypes"
 	"github.com/pritunl/pritunl-link/state"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 func AdvertiseRoutes() (err error) {
 	states := state.States
+	networks := []string{}
 
 	for _, stat := range states {
 		for _, link := range stat.Links {
 			for _, network := range link.RightSubnets {
+				networks = append(networks, network)
+
 				if config.Config.Provider == "aws" {
 					err = AwsAddRoute(network)
 					if err != nil {
@@ -24,6 +33,23 @@ func AdvertiseRoutes() (err error) {
 				}
 			}
 		}
+	}
+
+	err = os.MkdirAll(constants.VarDir, 0755)
+	if err != nil {
+		err = errortypes.WriteError{
+			errors.Wrap(err, "advertise: Failed to create var directory"),
+		}
+		return
+	}
+
+	data := strings.Join(networks, "\n")
+	err = ioutil.WriteFile(constants.RoutesPath, []byte(data), 0644)
+	if err != nil {
+		err = errortypes.WriteError{
+			errors.Wrap(err, "advertise: Failed to write routes"),
+		}
+		return
 	}
 
 	return
