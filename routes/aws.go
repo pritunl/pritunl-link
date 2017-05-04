12eb8e3648d8264e7c51525f8512f8813630ca7c
Zachary Huff <zach.huff.386@gmail.com>
2017-05-03 20:58:36 -0400

    Add routes

---
+++ routes/aws.go
@@ -0,0 +1,27 @@
+package routes
+
+type AwsRoute struct {
+	Network     string `json:"network"`
+	InterfaceId string `json:"interface_id"`
+	InstanceId  string `json:"instance_id"`
+}
+
+func (r *AwsRoute) Add() (err error) {
+	routes, err := GetCurrent()
+	if err != nil {
+		return
+	}
+
+	if routes.Aws == nil {
+		routes.Aws = map[string]*AwsRoute{}
+	}
+
+	routes.Aws[r.Network] = r
+
+	err = routes.Commit()
+	if err != nil {
+		return
+	}
+
+	return
+}
---
+++ routes/routes.go
@@ -0,0 +1,60 @@
+package routes
+
+import (
+	"encoding/json"
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/pritunl-link/constants"
+	"github.com/pritunl/pritunl-link/errortypes"
+	"io/ioutil"
+	"os"
+)
+
+type CurrentRoutes struct {
+	Aws map[string]*AwsRoute `json:"aws"`
+}
+
+func (c *CurrentRoutes) Commit() (err error) {
+	data, err := json.Marshal(c)
+	if err != nil {
+		err = errortypes.ParseError{
+			errors.Wrap(err, "advertise: Failed to prase routes"),
+		}
+		return
+	}
+
+	err = ioutil.WriteFile(constants.CurRoutesPath, data, 0644)
+	if err != nil {
+		err = errortypes.WriteError{
+			errors.Wrap(err, "advertise: Failed to write routes"),
+		}
+		return
+	}
+
+	return
+}
+
+func GetCurrent() (routes *CurrentRoutes, err error) {
+	routes = &CurrentRoutes{}
+
+	if _, e := os.Stat(constants.CurRoutesPath); os.IsNotExist(e) {
+		return
+	}
+
+	data, err := ioutil.ReadFile(constants.CurRoutesPath)
+	if err != nil {
+		err = errortypes.ReadError{
+			errors.Wrap(err, "advertise: Failed to read routes"),
+		}
+		return
+	}
+
+	err = json.Unmarshal(data, routes)
+	if err != nil {
+		err = errortypes.ParseError{
+			errors.Wrap(err, "advertise: Failed to prase routes"),
+		}
+		return
+	}
+
+	return
+}
