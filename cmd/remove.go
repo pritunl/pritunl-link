2821cb0b392dcd6e1f4b7c413b00f559dc00463c
Zachary Huff <zach.huff.386@gmail.com>
2017-05-07 21:39:32 -0400

    Add remove cmd

---
+++ cmd/remove.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/pritunl/pritunl-link/config"
+)
+
+func Remove(uri string) (err error) {
+	for i, u := range config.Config.Uris {
+		if uri == u {
+			config.Config.Uris = append(
+				config.Config.Uris[:i],
+				config.Config.Uris[i+1:]...,
+			)
+		}
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"uris": config.Config.Uris,
+	}).Info("cmd.remove: Removed URI")
+
+	return
+}
