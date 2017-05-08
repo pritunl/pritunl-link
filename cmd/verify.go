4e17396192c348337e75f1df2a738d8c00e918ae
Zachary Huff <zach.huff.386@gmail.com>
2017-05-07 21:44:36 -0400

    Add verify cmd

---
+++ cmd/verify.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/pritunl/pritunl-link/config"
+)
+
+func VerifyOn() (err error) {
+	config.Config.SkipVerify = false
+
+	err = config.Save()
+	if err != nil {
+		return
+	}
+
+	logrus.Info("cmd.verify: Certificate verification enabled")
+
+	return
+}
+
+func VerifyOff() (err error) {
+	config.Config.SkipVerify = true
+
+	err = config.Save()
+	if err != nil {
+		return
+	}
+
+	logrus.Info("cmd.verify: Certificate verification disable")
+
+	return
+}
