b3a2199828aba04bd0c559b3230edad0b4493710
Zachary Huff <zach.huff.386@gmail.com>
2017-05-17 20:23:51 -0400

    Wait in start cmd

--- cmd/start.go
+++ cmd/start.go
@@ -5,6 +5,7 @@ import (
 	"github.com/pritunl/pritunl-link/config"
 	"github.com/pritunl/pritunl-link/constants"
 	"github.com/pritunl/pritunl-link/sync"
+	"time"
 )
 
 func Start() (err error) {
@@ -14,6 +15,10 @@ func Start() (err error) {
 	}).Info("cmd.start: Starting link")
 
 	sync.Init()
+
+	for {
+		time.Sleep(1 * time.Second)
+	}
 
 	return
 }
