9ca54d182e8df4a699413d733da68a8a070aaf1f
Zachary Huff <zach.huff.386@gmail.com>
2017-05-20 00:09:56 -0400

    Fix sync init in start

--- cmd/start.go
+++ cmd/start.go
@@ -9,13 +9,13 @@ import (
 )
 
 func Start() (err error) {
+	sync.Init()
+
 	logrus.WithFields(logrus.Fields{
 		"version":        constants.Version,
 		"local_address":  state.GetLocalAddress(),
 		"public_address": state.GetPublicAddress(),
 	}).Info("cmd.start: Starting link")
-
-	sync.Init()
 
 	for {
 		time.Sleep(1 * time.Second)
