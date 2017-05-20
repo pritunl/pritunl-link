e4f8d99a94414e2100ac1b63cd5e439147417c9c
Zachary Huff <zach.huff.386@gmail.com>
2017-05-20 04:54:11 -0400

    Add disconnect timeout to constants

--- constants/constants.go
+++ constants/constants.go
@@ -1,13 +1,17 @@
 package constants
 
-import "path"
+import (
+	"path"
+	"time"
+)
 
 const (
-	Version         = "1.0.0"
-	VarDir          = "/var/lib/pritunl"
-	ConfPath        = "/etc/pritunl-link.json"
-	PublicIpServer  = "https://app.pritunl.com/ip"
-	PublicIp6Server = "https://app6.pritunl.com/ip"
+	Version            = "1.0.0"
+	VarDir             = "/var/lib/pritunl"
+	ConfPath           = "/etc/pritunl-link.json"
+	PublicIpServer     = "https://app.pritunl.com/ip"
+	PublicIp6Server    = "https://app6.pritunl.com/ip"
+	DiconnectedTimeout = 60 * time.Second
 )
 
 var (
