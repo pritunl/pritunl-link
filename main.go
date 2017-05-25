0cd675d39d9da0c3b0961e6baf4c9ca6b6b9807b
Zachary Huff <zach.huff.386@gmail.com>
2017-05-24 23:43:15 -0400

    Add help command

--- main.go
+++ main.go
@@ -2,101 +2,140 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/pritunl/pritunl-link/cmd"
 	"github.com/pritunl/pritunl-link/logger"
 	"github.com/pritunl/pritunl-link/requires"
 )
 
+const help = `
+Usage: pritunl-link COMMAND
+
+Commands:
+  start                     Start link service
+  add                       Add a Pritunl server URI
+  remove                    Remove a Pritunl server URI
+  clear                     Clear all configured Pritunl server URIs
+  local-address             Manually set local IP address
+  public-address            Manually set public IP address
+  verify-on                 Enable HTTPS certificate verification when connecting to Pritunl server
+  verify-off                Disable HTTPS certificate verification when connecting to Pritunl server
+  disconnected-timeout-on   Enable restart when disconnected for duration of timeout
+  disconnected-timeout-off  Disable restart when disconnected for duration of timeout
+  provider                  Manually set network provider
+  unifi-username            Set Unifi username
+  unifi-password            Set Unifi password
+  unifi-controller          Set URL of Unifi controller
+`
+
+func Init() {
+	logger.Init()
+	requires.Init()
+}
+
 func main() {
 	flag.Parse()
-	logger.Init()
-
-	requires.Init()
 
 	switch flag.Arg(0) {
 	case "start":
+		Init()
 		err := cmd.Start()
 		if err != nil {
 			panic(err)
 		}
 		break
 	case "add":
+		Init()
 		err := cmd.Add(flag.Arg(1))
 		if err != nil {
 			panic(err)
 		}
 		break
 	case "remove":
+		Init()
 		err := cmd.Remove(flag.Arg(1))
 		if err != nil {
 			panic(err)
 		}
 		break
 	case "clear":
+		Init()
 		err := cmd.Clear()
 		if err != nil {
 			panic(err)
 		}
 		break
 	case "local-address":
+		Init()
 		err := cmd.LocalAddress(flag.Arg(1))
 		if err != nil {
 			panic(err)
 		}
 		break
 	case "public-address":
+		Init()
 		err := cmd.PublicAddress(flag.Arg(1))
 		if err != nil {
 			panic(err)
 		}
 		break
 	case "verify-on":
+		Init()
 		err := cmd.VerifyOn()
 		if err != nil {
 			panic(err)
 		}
 		break
 	case "verify-off":
+		Init()
 		err := cmd.VerifyOff()
 		if err != nil {
 			panic(err)
 		}
 		break
 	case "disconnected-timeout-on":
+		Init()
 		err := cmd.DisconnectedTimeoutOn()
 		if err != nil {
 			panic(err)
 		}
 		break
 	case "disconnected-timeout-off":
+		Init()
 		err := cmd.DisconnectedTimeoutOff()
 		if err != nil {
 			panic(err)
 		}
 		break
 	case "provider":
+		Init()
 		err := cmd.Provider(flag.Arg(1))
 		if err != nil {
 			panic(err)
 		}
 		break
 	case "unifi-username":
+		Init()
 		err := cmd.UnifiUsername(flag.Arg(1))
 		if err != nil {
 			panic(err)
 		}
 		break
 	case "unifi-password":
+		Init()
 		err := cmd.UnifiPassword(flag.Arg(1))
 		if err != nil {
 			panic(err)
 		}
 		break
 	case "unifi-controller":
+		Init()
 		err := cmd.UnifiController(flag.Arg(1))
 		if err != nil {
 			panic(err)
 		}
 		break
+	default:
+		fmt.Println(help)
 	}
 }
