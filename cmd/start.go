e9ebf7081f60f6b6c982eb062e3f9956b7181c87
Zachary Huff <zach.huff.386@gmail.com>
2017-05-25 05:19:58 -0400

    Change init log message

--- cmd/start.go
+++ cmd/start.go
@@ -3,19 +3,16 @@ package cmd
 import (
 	"github.com/Sirupsen/logrus"
 	"github.com/pritunl/pritunl-link/constants"
-	"github.com/pritunl/pritunl-link/state"
 	"github.com/pritunl/pritunl-link/sync"
 	"time"
 )
 
 func Start() (err error) {
-	sync.Init()
-
 	logrus.WithFields(logrus.Fields{
-		"version":        constants.Version,
-		"local_address":  state.GetLocalAddress(),
-		"public_address": state.GetPublicAddress(),
+		"version": constants.Version,
 	}).Info("cmd.start: Starting link")
+
+	sync.Init()
 
 	for {
 		time.Sleep(1 * time.Second)
