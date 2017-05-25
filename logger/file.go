e815741e0ee5d40e5974a096519208ec5b8f967d
Zachary Huff <zach.huff.386@gmail.com>
2017-05-25 05:16:15 -0400

    Add file logger

---
+++ logger/file.go
@@ -0,0 +1,50 @@
+package logger
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/pritunl/pritunl-link/errortypes"
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/pritunl-link/constants"
+	"os"
+)
+
+func init() {
+	senders = append(senders, &fileSender{})
+}
+
+type fileSender struct{}
+
+func (s *fileSender) Init() {}
+
+func (s *fileSender) Parse(entry *logrus.Entry) {
+	err := s.send(entry)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error": err,
+		}).Error("logger: File send error")
+	}
+}
+
+func (s *fileSender) send(entry *logrus.Entry) (err error) {
+	msg := formatPlain(entry)
+
+	file, err := os.OpenFile(constants.LogPath,
+		os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		err = &errortypes.WriteError{
+			errors.Wrap(err, "logger: Failed to open log file"),
+		}
+		return
+	}
+	defer file.Close()
+
+	_, err = file.Write(msg)
+	if err != nil {
+		err = &errortypes.WriteError{
+			errors.Wrap(err, "logger: Failed to write to log file"),
+		}
+		return
+	}
+
+	return
+}
