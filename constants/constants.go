1ad43af6949e57cf7837b3c18fd93e6d2a1f5e78
Zachary Huff <zach.huff.386@gmail.com>
2017-06-16 02:13:41 -0400

    Create new release

--- constants/constants.go
+++ constants/constants.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	Version                   = "1.0.569.21"
+	Version                   = "1.0.570.25"
 	VarDir                    = "/var/lib/pritunl_link"
 	LogPath                   = "/var/log/pritunl_link.log"
 	ConfPath                  = "/etc/pritunl_link.json"
