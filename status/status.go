8a72311988be8888e045130f85a1863cb51434cd
Zachary Huff <zach.huff.386@gmail.com>
2018-05-31 09:28:25 -0400

    Fix status get

--- status/status.go
+++ status/status.go
@@ -17,60 +17,46 @@ func Get() (status Status, connected int, err error) {
 		return
 	}
 
-	isIkeState := false
-	ikeState := ""
-
 	for _, line := range strings.Split(output, "\n") {
 		lines := strings.SplitN(line, ":", 2)
 		if len(lines) != 2 {
 			continue
 		}
 
-		isIkeState = strings.HasSuffix(lines[0], "]")
+		if !strings.HasSuffix(lines[0], "]") {
+			continue
+		}
 
-		if isIkeState {
-			ikeState = strings.SplitN(
-				strings.TrimSpace(lines[1]), " ", 2)[0]
-		} else {
-			if !strings.Contains(lines[1], "reqid") {
-				continue
-			}
+		connId := strings.SplitN(strings.SplitN(lines[0], "[", 2)[0], "-", 2)
+		connState := strings.SplitN(
+			strings.TrimSpace(lines[1]), " ", 2)[0]
 
-			connId := strings.SplitN(strings.SplitN(
-				lines[0], "{", 2)[0], "-", 2)
-			connState := strings.SplitN(
-				strings.TrimSpace(lines[1]), ",", 2)[0]
+		if len(connId) != 2 {
+			continue
+		}
 
-			if len(connId) != 2 {
-				continue
-			}
+		switch connState {
+		case "ESTABLISHED":
+			connState = "connected"
+			break
+		case "CONNECTING":
+			connState = "connecting"
+			break
+		default:
+			connState = "disconnected"
+		}
 
-			switch ikeState {
-			case "ESTABLISHED":
-				if connState == "INSTALLED" {
-					connState = "connected"
-				} else {
-					connState = "disconnected"
-				}
-				break
-			case "CONNECTING":
-				connState = "connecting"
-			default:
-				connState = "disconnected"
-			}
+		if _, ok := status[connId[0]]; !ok {
+			status[connId[0]] = map[string]string{}
+		}
 
-			if _, ok := status[connId[0]]; !ok {
-				status[connId[0]] = map[string]string{}
-			}
+		if _, ok := status[connId[0]][connId[1]]; !ok {
+			status[connId[0]][connId[1]] = connState
+		} else if (status[connId[0]][connId[1]] == "disconnected") ||
+			(status[connId[0]][connId[1]] == "connecting" &&
+				connState == "connected") {
 
-			if _, ok := status[connId[0]][connId[1]]; !ok {
-				status[connId[0]][connId[1]] = connState
-			} else if (status[connId[0]][connId[1]] == "disconnected") ||
-				(status[connId[0]][connId[1]] == "connecting" &&
-					connState == "connected") {
-
-				status[connId[0]][connId[1]] = connState
-			}
+			status[connId[0]][connId[1]] = connState
 		}
 	}
 
