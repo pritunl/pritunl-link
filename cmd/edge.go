fba8f82d36f99cebe176ec3bb2598689244a5ead
Zachary Huff <zach.huff.386@gmail.com>
2018-12-01 14:31:22 -0500

    Add edge cmd

---
+++ cmd/edge.go
@@ -0,0 +1,77 @@
+package cmd
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/pritunl/pritunl-link/config"
+)
+
+func EdgeUsername(username string) (err error) {
+	config.Config.Edge.Username = username
+
+	err = config.Save()
+	if err != nil {
+		return
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"username": config.Config.Edge.Username,
+	}).Info("cmd.unifi: Set EdgeRouter username")
+
+	return
+}
+
+func EdgePassword(password string) (err error) {
+	config.Config.Edge.Password = password
+
+	err = config.Save()
+	if err != nil {
+		return
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"password": config.Config.Edge.Password,
+	}).Info("cmd.unifi: Set EdgeRouter password")
+
+	return
+}
+
+func EdgeHostname(hostname string) (err error) {
+	config.Config.Edge.Hostname = hostname
+
+	err = config.Save()
+	if err != nil {
+		return
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"hostname": config.Config.Edge.Hostname,
+	}).Info("cmd.unifi: Set EdgeRouter hostname")
+
+	return
+}
+
+func EdgePortOn() (err error) {
+	config.Config.Edge.DisablePort = false
+
+	err = config.Save()
+	if err != nil {
+		return
+	}
+
+	logrus.Info("cmd.unifi: EdgeRouter port forwarding enabled")
+
+	return
+}
+
+func EdgePortOff() (err error) {
+	config.Config.Edge.DisablePort = true
+
+	err = config.Save()
+	if err != nil {
+		return
+	}
+
+	logrus.Info("cmd.unifi: EdgeRouter port forwarding disabled")
+
+	return
+}
