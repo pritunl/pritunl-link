1dada97afd2816e54ae8afef7846ede5edcebc3f
Zachary Huff <zach.huff.386@gmail.com>
2021-09-21 00:10:57 -0400

    Add allow port to iptables

--- iptables/iptables.go
+++ iptables/iptables.go
@@ -1,12 +1,20 @@
 package iptables
 
 import (
-	"github.com/pritunl/pritunl-link/utils"
 	"strings"
+
+	"github.com/pritunl/pritunl-link/utils"
 )
 
-func ClearIpTables() (err error) {
-	output, err := utils.ExecOutput("", "iptables-save")
+func clearIpTables(match string, ipv6 bool) (err error) {
+	var iptablesExec string
+	if ipv6 {
+		iptablesExec = "ip6tables"
+	} else {
+		iptablesExec = "iptables"
+	}
+
+	output, err := utils.ExecOutput("", iptablesExec+"-save")
 	if err != nil {
 		return
 	}
@@ -25,7 +33,7 @@ func ClearIpTables() (err error) {
 			continue
 		}
 
-		if !strings.Contains(line, "--comment pritunl-zero") {
+		if !strings.Contains(line, match) {
 			continue
 		}
 
@@ -35,10 +43,14 @@ func ClearIpTables() (err error) {
 		fields := strings.Fields(line)[1:]
 		args = append(args, fields...)
 
-		utils.Exec("", "iptables", args...)
+		utils.Exec("", iptablesExec, args...)
 	}
 
 	return
+}
+
+func ClearIpTables() (err error) {
+	return clearIpTables("--comment pritunl-link-direct", false)
 }
 
 func UpsertRule(table string, rule ...string) (err error) {
@@ -53,6 +65,108 @@ func UpsertRule(table string, rule ...string) (err error) {
 		err = utils.Exec("", "iptables", args...)
 		if err != nil {
 			return
+		}
+	}
+
+	return
+}
+
+func AllowPort(source, port, proto string) (err error) {
+	var iptablesExec string
+	if strings.Contains(source, ":") {
+		iptablesExec = "ip6tables"
+	} else {
+		iptablesExec = "iptables"
+	}
+
+	rule := []string{
+		"INPUT", "1",
+		"-p", proto,
+		"-m", proto,
+		"--dport", port,
+		"-s", source,
+		"-j", "ACCEPT",
+		"-m", "comment",
+		"--comment", "pritunl-link-accept",
+	}
+
+	args := []string{"-C"}
+	args = append(args, rule...)
+
+	e := utils.ExecSilent("", iptablesExec, args...)
+	if e != nil {
+		args = []string{"-I"}
+		args = append(args, rule...)
+
+		err = utils.Exec("", iptablesExec, args...)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+func DisallowPort(source, port, proto string) (err error) {
+	var iptablesExec string
+	if strings.Contains(source, ":") {
+		iptablesExec = "ip6tables"
+	} else {
+		iptablesExec = "iptables"
+	}
+
+	rule := []string{
+		"INPUT",
+		"-p", proto,
+		"-m", proto,
+		"--dport", port,
+		"-s", source,
+		"-j", "ACCEPT",
+		"-m", "comment",
+		"--comment", "pritunl-link-accept",
+	}
+
+	args := []string{"-C"}
+	args = append(args, rule...)
+
+	e := utils.ExecSilent("", iptablesExec, args...)
+	if e == nil {
+		args = []string{"-D"}
+		args = append(args, rule...)
+
+		err = utils.Exec("", iptablesExec, args...)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+func DropPort(port, proto string) (err error) {
+	rule := []string{
+		"INPUT",
+		"-p", proto,
+		"-m", proto,
+		"--dport", port,
+		"-j", "DROP",
+		"-m", "comment",
+		"--comment", "pritunl-link-drop",
+	}
+
+	for _, iptablesExec := range []string{"iptables", "ip6tables"} {
+		args := []string{"-C"}
+		args = append(args, rule...)
+
+		e := utils.ExecSilent("", iptablesExec, args...)
+		if e != nil {
+			args = []string{"-A"}
+			args = append(args, rule...)
+
+			err = utils.Exec("", iptablesExec, args...)
+			if err != nil {
+				return
+			}
 		}
 	}
 
