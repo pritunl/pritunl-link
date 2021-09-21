979a35ba99c4be9526b507559ec5be53d209ee00
Zachary Huff <zach.huff.386@gmail.com>
2021-09-21 00:33:01 -0400

    Set sysctl in utils

--- utils/sysctl.go
+++ utils/sysctl.go
@@ -1,5 +1,14 @@
 package utils
 
+import (
+	"fmt"
+	"net"
+	"strings"
+
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/pritunl-link/errortypes"
+)
+
 func NetInit() (err error) {
 	err = ExecSilent(
 		"", "sysctl", "-w", "net.ipv4.ip_forward=1")
@@ -31,8 +40,27 @@ func NetInit() (err error) {
 		return
 	}
 
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		err = &errortypes.ReadError{
+			errors.Wrap(err, "utils: Failed to read network interfaces"),
+		}
+		return
+	}
+
+	ExecSilent("", "sysctl", "-w", "net.ipv6.conf.all.accept_ra=2")
+	ExecSilent("", "sysctl", "-w", "net.ipv6.conf.default.accept_ra=2")
 	ExecSilent("", "sysctl", "-w", "net.ipv6.conf.all.forwarding=1")
 	ExecSilent("", "sysctl", "-w", "net.ipv6.conf.default.forwarding=1")
+
+	for _, iface := range ifaces {
+		if strings.HasPrefix(iface.Name, "lo") {
+			continue
+		}
+
+		ExecSilent("", "sysctl", "-w",
+			fmt.Sprintf("net.ipv6.conf.%s.accept_ra=2", iface.Name))
+	}
 
 	return
 }
