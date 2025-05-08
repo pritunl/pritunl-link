333c5d9a9cacc0d34c6edefa7ac7e349694221b2
Zachary Huff <zach.huff.386@gmail.com>
2025-05-08 07:09:09 -0400

    Update wg templates in ipsec constants

--- ipsec/constants.go
+++ ipsec/constants.go
@@ -37,16 +37,17 @@ const (
 	ikeCiphers         = "aes128-sha256-x25519,aes128-sha256-curve25519,aes128-sha256-modp2048s256,aes128-sha256-ecp256,aes128-sha256-modp3072,aes192-sha384-modp2048s256,aes192-sha384-ecp384,aes192-sha384-curve25519,aes256-sha512-modp2048s256,aes256-sha512-ecp521,aes256-sha512-curve25519,aes128-sha256-modp4096,aes128-sha256-modp2048,aes128-sha256-modp1536,aes128-sha1-modp2048s256,aes128-sha1-ecp256,aes128-sha1-modp3072,aes128-sha1-curve25519,aes128-sha1-modp4096,aes128-sha1-modp3072,aes128-sha1-modp2048,aes128-sha1-modp1536"
 	secretsTemplateStr = `{{.Left}} {{.Right}} : PSK "{{.PreSharedKey}}"
 `
-	confWgTemplateStr = `[Interface]
+	confWgTemplateStr = `# pritunl-link:{{.WgHash}}
+[Interface]
 PrivateKey = {{.WgPrivateKey}}
-ListenPort = 8273
+ListenPort = {{.WgPort}}
 `
 	confWgPeerTemplateStr = `
 [Peer]
 PublicKey = {{.WgPublicKey}}
 PresharedKey = {{.WgPreSharedKey}}
 AllowedIPs = {{.RightSubnets}}
-Endpoint = {{.RightWg}}:8273
+Endpoint = {{.RightWg}}:{{.WgPort}}
 PersistentKeepalive = 25
 `
 )
