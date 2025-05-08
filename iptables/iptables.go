f47cc3e69c0d233f6a39af070c9ca04feaf461cf
Zachary Huff <zach.huff.386@gmail.com>
2025-05-08 07:08:00 -0400

    Add ipset functions to iptables

--- iptables/iptables.go
+++ iptables/iptables.go
@@ -173,7 +173,134 @@ func DropPort(port, proto string) (err error) {
 	return
 }
 
+func AllowPortSet(source, portSet, proto string) (err error) {
+	var iptablesExec string
+	if strings.Contains(source, ":") {
+		iptablesExec = "ip6tables"
+	} else {
+		iptablesExec = "iptables"
+	}
+
+	rule := []string{
+		"INPUT", "1",
+		"-p", proto,
+		"-m", "set",
+		"--match-set", portSet, "dst",
+		"-s", source,
+		"-j", "ACCEPT",
+		"-m", "comment",
+		"--comment", "pritunl-link-accept",
+	}
+
+	args := []string{"-C"}
+	args = append(args, rule...)
+
+	e := utils.ExecSilent("", iptablesExec, args...)
+	if e != nil {
+		args = []string{"-I"}
+		args = append(args, rule...)
+
+		err = utils.Exec("", iptablesExec, args...)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+func DisallowPortSet(source, portSet, proto string) (err error) {
+	var iptablesExec string
+	if strings.Contains(source, ":") {
+		iptablesExec = "ip6tables"
+	} else {
+		iptablesExec = "iptables"
+	}
+
+	rule := []string{
+		"INPUT",
+		"-p", proto,
+		"-m", "set",
+		"--match-set", portSet, "dst",
+		"-s", source,
+		"-j", "ACCEPT",
+		"-m", "comment",
+		"--comment", "pritunl-link-accept",
+	}
+
+	args := []string{"-C"}
+	args = append(args, rule...)
+
+	e := utils.ExecSilent("", iptablesExec, args...)
+	if e == nil {
+		args = []string{"-D"}
+		args = append(args, rule...)
+
+		err = utils.Exec("", iptablesExec, args...)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+func DropPortSet(portSet, proto string) (err error) {
+	rule := []string{
+		"INPUT",
+		"-p", proto,
+		"-m", "set",
+		"--match-set", portSet, "dst",
+		"-j", "DROP",
+		"-m", "comment",
+		"--comment", "pritunl-link-drop",
+	}
+
+	for _, iptablesExec := range []string{"iptables", "ip6tables"} {
+		args := []string{"-C"}
+		args = append(args, rule...)
+
+		e := utils.ExecSilent("", iptablesExec, args...)
+		if e != nil {
+			args = []string{"-A"}
+			args = append(args, rule...)
+
+			err = utils.Exec("", iptablesExec, args...)
+			if err != nil {
+				return
+			}
+		}
+	}
+
+	return
+}
+
+func InitWgIpset() (err error) {
+	output, err := utils.ExecOutput("", "ipset", "list", "-n")
+	if err != nil {
+		return
+	}
+
+	for _, item := range strings.Split(output, "\n") {
+		if strings.TrimSpace(item) == "wgp" {
+			return
+		}
+	}
+
+	err = utils.Exec("", "ipset", "create", "wgp",
+		"bitmap:port", "range", "0-65535")
+	if err != nil {
+		return
+	}
+
+	return
+}
+
 func InitAcceptIpTables() (err error) {
+	err = InitWgIpset()
+	if err != nil {
+		return
+	}
 	err = DropPort("500", "udp")
 	if err != nil {
 		return
@@ -182,11 +309,11 @@ func InitAcceptIpTables() (err error) {
 	if err != nil {
 		return
 	}
-	err = DropPort("8273", "udp")
+	err = DropPort("9790", "tcp")
 	if err != nil {
 		return
 	}
-	err = DropPort("9790", "tcp")
+	err = DropPortSet("wgp", "udp")
 	if err != nil {
 		return
 	}
